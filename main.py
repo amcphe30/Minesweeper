7f9fdedfd701ece21e62943aaf588ed6dc3d4c6f
Abigail McPhee <abby@mcpheefamily.ca>
2023-12-16 23:16:31 -0800

    it's minesweeper!

--- main.py
+++ main.py
@@ -14,11 +14,14 @@ def on_click(x, y):
       for label in labels: label.destroy()
       draw_images()
 
+# make a list of sand number images to you don't need so many elifs
+# different colours for each number
+
 
 root = tk.Tk()
 root.title("minesweeper")
 width, height = 300, 300
-center_window(root, width, height)
+center_window(root, width + 21, height + 21)
 tile_dimension = int(width / minesweeper.rows)
 
 # Add widgets (buttons, labels, etc.) and other elements here
@@ -27,7 +30,10 @@ for r in range(minesweeper.rows):
         for c in range(minesweeper.columns):
              r + c
 
-def draw_images():
+sand_x_images = []
+
+# -1 covered no mine, -2 covered mine, -3 uncovered mine, otherwise num of mines around
+def draw_images():  
     grass_image = Image.open("grass.png")
     grass_image = grass_image.resize((tile_dimension, tile_dimension))
     grass = ImageTk.PhotoImage(grass_image)
@@ -37,24 +43,29 @@ def draw_images():
     bomb_image = Image.open("bomb.png")
     bomb_image = bomb_image.resize((tile_dimension, tile_dimension))
     bomb = ImageTk.PhotoImage(bomb_image)
+    for x in range(0, 9):
+         file_name = ("sand-" + str(x) + ".png")
+         sand_image_x = Image.open(file_name)
+         sand_image_x = sand_image_x.resize((tile_dimension, tile_dimension))
+         sand_x_images.append(ImageTk.PhotoImage(sand_image_x))  
     onclick = lambda x, y: (lambda p: on_click(x, y))
     for r in range(minesweeper.rows):
         for c in range(minesweeper.columns):
-            if mines[r][c] == 0 or mines[r][c] == 1: # covered
+            if mines[r][c] == -1 or mines[r][c] == -2: # covered
                 label1 = tk.Label(image = grass)
                 label1.image = grass
                 label1.bind('<Button-1>', onclick(r, c))
                 labels.append(label1)
-            elif mines[r][c] == 2: # uncovered sand
-                label1 = tk.Label(image = sand)
-                label1.image = sand
-                label1.bind('<Button-1>', onclick(r, c))
-                labels.append(label1)
-            elif mines[r][c] == 3: # uncovered bomb
+            elif mines[r][c] == -3: # uncovered bomb
                 label1 = tk.Label(image = bomb)
                 label1.image = bomb
                 label1.bind('<Button-1>', onclick(r, c))
                 labels.append(label1)
+            else: # uncovered sand
+                index = mines[r][c]
+                label1 = tk.Label(image = sand_x_images[index])
+                label1.image = sand_x_images[index]
+                labels.append(label1)    
             label1.grid(row = r, column = c)
 
 labels = []
--- minesweeper.py
+++ minesweeper.py
@@ -7,12 +7,17 @@ num_of_mines = 5
 game_over = False
 num_mines_left = num_of_mines
 
-grid = [[0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0]]
-# 0 covered no mine, 1 covered mine, 2 uncovered sand, 3 uncovered mine
+grid = [[-1, -1, -1, -1, -1],
+        [-1, -1, -1, -1, -1],
+        [-1, -1, -1, -1, -1],
+        [-1, -1, -1, -1, -1],
+        [-1, -1, -1, -1, -1]]
+num_around = [[-1, -1, -1, -1, -1],
+        [-1, -1, -1, -1, -1],
+        [-1, -1, -1, -1, -1],
+        [-1, -1, -1, -1, -1],
+        [-1, -1, -1, -1, -1]]
+# -1 covered no mine, -2 covered mine, -3 uncovered mine, otherwise num of mines around
 
 def init_mines():
     min = 1
@@ -25,7 +30,21 @@ def init_mines():
     for r in range(rows):
         for c in range(columns):
             if r * 5 + c + 1 in mines:
-                grid[r][c] = 1
+                grid[r][c] = -2
+
+def get_num_around(row, column):
+    if grid[row][column] == -2 or grid[row][column] == -3:
+        return grid[row][column]
+    total = 0
+    posR = [row - 1, row, row + 1]
+    posC = [column - 1, column, column + 1]
+    for r in posR:
+        for c in posC:
+            if r >= 0 and r < rows and c >= 0 and c < columns:
+                if grid[r][c] == -2 or grid[r][c] == -3:
+                    total += 1
+                
+    return total
 
 def print_mines():
     for row in grid:
@@ -38,12 +57,23 @@ def get_mines():
 
 def dig(r, c):
    print("pressed at (" + str(r) + "," + str(c) + ")")
-   if grid[r][c] == 1:
-       grid[r][c] = 3
+   if grid[r][c] == -2:
+       grid[r][c] = -3
        print("game over")
    else:
-       grid[r][c] = 2
-       print("no bomb")
+       grid[r][c] = get_num_around(r, c)
+       print("safe, " + str(grid[r][c]) + " bombs around")
+
+
 
 init_mines()
 print_mines()
+
+for r in range(rows):
+        for c in range(columns):
+            num_around[r][c] = get_num_around(r, c)
+
+for row in num_around:
+        for element in row:
+            print(element, end=" ")
+        print() 
